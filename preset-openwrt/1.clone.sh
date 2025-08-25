15eeb45341e52c0493648e46c4d39e0d15247a80
XIK_ <104570346+Qixia00000@users.noreply.github.com>
2025-08-25 22:06:45 +0800

    Update 1.clone.sh

--- preset-openwrt/1.clone.sh
+++ preset-openwrt/1.clone.sh
@@ -1,30 +1,18 @@
 #!/usr/bin/env bash
 
-# download base code
+# 1. 拉取 OpenWrt 主线源码
 CODE_DIR=_firmware_code
 CODE_URL=https://git.openwrt.org/openwrt/openwrt.git
 CODE_BRANCH=openwrt-24.10
-SWITCH_LATEST_TAG=false
 git clone --single-branch -b $CODE_BRANCH $CODE_URL $CODE_DIR
-if $SWITCH_LATEST_TAG; then
-    cd $CODE_DIR
-    LATEST_TAG_HASH=$(git rev-list --tags --max-count=1)
-    if [ -z "$LATEST_TAG_HASH" ]; then
-        echo "No tag to switch, keep the latest commit"
-    else
-        git checkout $LATEST_TAG_HASH
-        LATEST_TAG=$(git describe --tags $LATEST_TAG_HASH)
-        echo "The code has been switched to the latest stable version $LATEST_TAG"
-    fi
-    cd ..
-fi
 mv ./$CODE_DIR/* ./
 
-# download app codes
+# 2. 建立本地 feeds 目录
 SUPPLY_DIR=_supply_packages
 echo "src-link supply $PWD/$SUPPLY_DIR" >> feeds.conf.default
-mkdir $SUPPLY_DIR && cd $SUPPLY_DIR
+mkdir -p $SUPPLY_DIR && cd $SUPPLY_DIR
+
+# 以下仅 clone 你在 .config 里真正用到的插件
 git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon.git
-
-
-
+git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config.git
+# adblock / adblock-fast / ddns / dnscrypt-proxy 等官方源已自带，无需额外 clone
